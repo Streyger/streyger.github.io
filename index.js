e00110de23b97bdaa4524a6b04c6a79b36675055
Serge T <29712112+Streyger@users.noreply.github.com>
2017-08-03 11:48:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function purpose(){
+location.href = "#purpose";
+}
+
+function certifications(){
+location.href = "#certifications";
+}
+
+function skills(){
+location.href = "#skills";
+}
+
+function accomplishments(){
+location.href = "#accomplishments";
+}
+
+function contact_information(){
+location.href = "#contact_information";
+}
